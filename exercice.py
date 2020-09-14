9aa3e2a93d53342c42849059d04b7b63ca7e583b
julien-milosz <71023561+julien-milosz@users.noreply.github.com>
2020-09-14 14:22:31 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -6,11 +6,11 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return ((a+b+c)/3)
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return ((angle_degs + (angle_mins/60) + (angle_secs/3600))*(math.pi/180))
 
 
 def to_degrees(angle_rads: float) -> tuple:
@@ -18,11 +18,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return ((temperature - 32)*(5/9))
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return ((temperature*(9/5))+32)
 
 
 def main() -> None:
